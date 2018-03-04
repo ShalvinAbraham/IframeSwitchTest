0b3839be8b1bf013b1248d311f151261892cd849
Shalvin <sat4star@gmail.com>
2018-03-04 15:45:41 +0000

    Support for Retry with a wait time in between

--- IframeSwitchTest/ElementFinder.cs
+++ IframeSwitchTest/ElementFinder.cs
@@ -1,15 +1,20 @@
 ﻿using OpenQA.Selenium;
+using System;
+using System.Threading;
 
 namespace IframeSwitchTest
 {
     public static class ElementFinder
     {
-        public static IWebElement FindInnerElement(this IWebDriver driver, By by, bool switchDriverToDefaultContent = true)
+        public static IWebElement FindInnerElement(this IWebDriver driver, By by, int retryCount = 2, int retryPauseTimeInSeconds = 30)
         {
             IWebElement elem = null;
 
-            if (switchDriverToDefaultContent)
+            if (retryCount > 0)
+            {
                 driver.SwitchTo().DefaultContent();
+                Console.WriteLine($"{DateTime.Now.ToString()} : Switching to DefaultContent");
+            }
 
             var elems = driver.FindElements(by);
 
@@ -23,15 +28,27 @@ namespace IframeSwitchTest
 
                 foreach (var iframe in iframes)
                 {
+                    Console.WriteLine($"{DateTime.Now.ToString()} : Switching to inner iframe");
                     driver.SwitchTo().Frame(iframe);
-                    elem = driver.FindInnerElement(by, false);
+                    elem = driver.FindInnerElement(by, 0);
 
                     if (elem != null)
                         break;
 
+                    Console.WriteLine($"{DateTime.Now.ToString()} : Switching Back");
                     driver.SwitchTo().ParentFrame();
                 }
             }
+
+            if (elem == null && retryCount > 0)
+            {
+                Console.WriteLine($"{DateTime.Now.ToString()} : Did not find the element [{by.ToString()}], waiting for {retryPauseTimeInSeconds} sec and trying {retryCount} more times");
+                Thread.Sleep(retryPauseTimeInSeconds * 1000);
+                elem = driver.FindInnerElement(by, --retryCount, retryPauseTimeInSeconds);
+            }
+
+            if (elem != null)
+                Console.WriteLine($"{DateTime.Now.ToString()} : Got the element [{by.ToString()}] !!");
 
             return elem;
         }
--- IframeSwitchTest/Program.cs
+++ IframeSwitchTest/Program.cs
@@ -13,14 +13,16 @@ namespace IframeSwitchTest
 
             driver.Navigate().GoToUrl("https://sat4star.wixsite.com/cycle/product-page/i-m-a-product-3");
             driver.Manage().Window.Maximize();
+
             Thread.Sleep(10000);
 
             var subscriberEmail = driver.FindInnerElement(By.Name("subscriberEmail"));
             subscriberEmail.SendKeys("Test Email");
 
             var comment = driver.FindInnerElement(By.Name("comment"));
-            comment.Click();
             comment.SendKeys("Test Comment");
+
+            var nonExistingElement = driver.FindInnerElement(By.Name("nonExistingElement"), 10, 1);
 
             Console.WriteLine("Done. Press any key...");
             Console.Read();
